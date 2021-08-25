687cfedf13024708340b61db3d671dd240343503
lesley_w <lrwesley92@gmail.com>
2021-08-24 20:37:41 -0500

    Added email verification

---
+++ js/script.js
@@ -0,0 +1,59 @@
+//Selects submit button
+
+const button = document.querySelector("button");
+
+//Selects "Must be valid email" message
+
+const validateMessage = document.querySelector(".validate-message");
+console.log(validateMessage);
+//Selects email input
+
+const emailInput = document.querySelector("input");
+
+//Email Specifications
+
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+//================================================
+
+//Adds validation of email when button is clicked
+
+button.addEventListener("click", function () {
+  validateEmail();
+})
+
+//===============================================
+
+//Adds validation of email when "enter" is hit
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    validateEmail();
+  }
+})
+
+
+
+//==============================================
+
+ //Validates email
+
+ const validateEmail = function () {
+   const email = emailInput.value;
+
+   if (email.match(mailformat)) {
+     clearInput();
+   } else {
+     validateMessage.classList.remove("hide");
+   }
+ }
+
+//==============================================
+
+//Clears Input
+
+const clearInput = function () {
+  emailInput.value = "";
+}
+
+//================================================
